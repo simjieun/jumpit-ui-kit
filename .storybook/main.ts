9a534d83206bd652e2657e8a4917961f1f8c564c
simjiuen <simjiuen@gmail.com>
2023-06-28 13:54:12 +0900

    storybook builder를 webpack5로 변경

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,26 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/react-webpack5";
 const config: StorybookConfig = {
+  framework: "@storybook/react-webpack5",
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  features: {
+    storyStoreV7: false,
   },
   core: {
-    builder: "@storybook/builder-vite",
+    builder: {
+      name: "@storybook/builder-webpack5",
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
   },
   docs: {
     autodocs: "tag",
-  },
-  async viteFinal(config, options) {
-    // Add your configuration here
-    return config;
   },
 };
 export default config;
--- docs/assets/preview-dfd00437.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-c11b92b6.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-e891e943.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-e891e943.js","./iframe-c11b92b6.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-63c5d83d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-dfd00437.js.map
