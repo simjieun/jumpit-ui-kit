6eabd8e4d1c80c5c094258b8e4ba1b7c90e132aa
simjieun <simjiuen@gmail.com>
2023-06-30 12:02:04 +0900

    storybook re build

---
+++ docs/atoms-Button-stories-Button-stories.a13f91b6.iframe.bundle.js
@@ -0,0 +1,86 @@
+"use strict";(self.webpackChunkjumpit_ui_kit=self.webpackChunkjumpit_ui_kit||[]).push([[890],{"./src/components/atoms/Button/stories/Button.stories.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{LargePrimary:()=>LargePrimary,__namedExportsOrder:()=>__namedExportsOrder,default:()=>Button_stories});var styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js");const Button=styled_components_browser_esm.ZP.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  ${({size})=>size&&(size=>{switch(size){case"tiny":return styled_components_browser_esm.iv`
+        min-width: 60px;
+        height: 28px;
+        padding: 0 8px;
+        font-weight: 700;
+        font-size: 13px;
+        line-height: 20px;
+      `;case"small":return styled_components_browser_esm.iv`
+        min-width: 82px;
+        height: 36px;
+        padding: 0 16px;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 22px;
+      `;case"medium":return styled_components_browser_esm.iv`
+        min-width: 82px;
+        height: 40px;
+        padding: 0 16px;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 22px;
+      `;case"large":return styled_components_browser_esm.iv`
+        min-width: 82px;
+        height: 48px;
+        padding: 0 16px;
+        font-weight: 700;
+        font-size: 15px;
+        line-height: 24px;
+      `;case"xlarge":return styled_components_browser_esm.iv`
+        min-width: 82px;
+        height: 56px;
+        padding: 0 16px;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 24px;
+      `;case"xxlarge":return styled_components_browser_esm.iv`
+        min-width: 82px;
+        height: 64px;
+        padding: 0 16px;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 28px;
+      `;default:return null}})(size)}
+  ${({colorScheme})=>(colorScheme=>{switch(colorScheme){case"primary":return styled_components_browser_esm.iv`
+        background: #00dd6d;
+        color: #fff;
+        &:hover {
+          background: #01c662;
+        }
+      `;case"black":return styled_components_browser_esm.iv`
+        background: #000;
+        color: #fff;
+        &:hover {
+          background: #333;
+        }
+      `;case"gray":return styled_components_browser_esm.iv`
+        background: #e4e4e4;
+        color: #000;
+        &:hover {
+          background: #d4d4d4;
+        }
+      `;case"white":return styled_components_browser_esm.iv`
+        background: #fff;
+        color: #000;
+        &:hover {
+          background: #f8f8f8;
+        }
+      `;default:return null}})(colorScheme)}
+  ${({variant})=>variant&&(variant=>{switch(variant){case"filled":case"link":return styled_components_browser_esm.iv`
+        border: none;
+      `;case"outline":return styled_components_browser_esm.iv`
+        border: 1px solid #d4d4d4;
+      `;default:return null}})(variant)}
+  ${({shape})=>shape&&(shape=>{switch(shape){case"square":return styled_components_browser_esm.iv`
+        border-radius: 0;
+      `;case"rounded":return styled_components_browser_esm.iv`
+        border-radius: 8px;
+      `;case"circle":return styled_components_browser_esm.iv`
+        border-radius: 50%;
+      `;default:return null}})(shape)}
+`;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const Button_Button=({size,colorScheme="primary",variant,shape,isDisabled,children,onClick})=>(0,jsx_runtime.jsx)(Button,{size,colorScheme,variant,shape,isDisabled,onClick,children});Button_Button.displayName="Button";try{Button_Button.displayName="Button",Button_Button.__docgenInfo={description:"",displayName:"Button",props:{size:{defaultValue:null,description:"",name:"size",required:!0,type:{name:"enum",value:[{value:'"tiny"'},{value:'"small"'},{value:'"medium"'},{value:'"large"'},{value:'"xlarge"'},{value:'"xxlarge"'}]}},colorScheme:{defaultValue:{value:"primary"},description:"",name:"colorScheme",required:!1,type:{name:"enum",value:[{value:'"primary"'},{value:'"black"'},{value:'"gray"'},{value:'"white"'}]}},variant:{defaultValue:null,description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:'"filled"'},{value:'"outline"'},{value:'"link"'}]}},shape:{defaultValue:null,description:"",name:"shape",required:!1,type:{name:"enum",value:[{value:'"square"'},{value:'"rounded"'},{value:'"circle"'}]}},isDisabled:{defaultValue:null,description:"",name:"isDisabled",required:!1,type:{name:"boolean"}},onClick:{defaultValue:null,description:"",name:"onClick",required:!1,type:{name:"(() => void)"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/atoms/Button/index.tsx#Button"]={docgenInfo:Button_Button.__docgenInfo,name:"Button",path:"src/components/atoms/Button/index.tsx#Button"})}catch(__react_docgen_typescript_loader_error){}const Button_stories={title:"@jumpit-ui-kit/Button",component:Button_Button,tags:["autodocs"],argTypes:{size:{control:"select",options:["tiny","small","medium","large","xlarge","xxlarge"]},colorScheme:{control:"select",options:["primary","black","gray","white"]},variant:{control:"inline-radio",options:["filled","outline","link"]},shape:{control:"inline-radio",options:["square","rounded","circle"]},isDisabled:{control:"inline-radio",options:[!0,!1]}}},LargePrimary={args:{size:"large",colorScheme:"primary",variant:"filled",shape:"rounded",children:"Button"}};LargePrimary.parameters={...LargePrimary.parameters,docs:{...LargePrimary.parameters?.docs,source:{originalSource:'{\n  args: {\n    size: "large",\n    colorScheme: "primary",\n    variant: "filled",\n    shape: "rounded",\n    children: "Button"\n  }\n}',...LargePrimary.parameters?.docs?.source}}};const __namedExportsOrder=["LargePrimary"]}}]);
+//# sourceMappingURL=atoms-Button-stories-Button-stories.a13f91b6.iframe.bundle.js.map
